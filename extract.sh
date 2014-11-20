ed78062246c11fd2526731bcd0a867037136a26e
Timo Saarinen <timo.l.saarinen@aalto.fi>
2014-11-20 17:42:07 +0200

    Still a small fix to extract.sh; Add test-run2.csv and .ods

--- extract.sh
+++ extract.sh
@@ -30,7 +30,7 @@ do
 		do
 			datetime=$(echo $file | cut -d'/' -f 3 | cut -d'_' -f 2 | cut -d'.' -f 1-3)
 			avg=$(cat $file | grep Average | awk '{print $9}' | cut -c1-2)
-			if [ $avg="" ]
+			if [ "$avg" == "" ]
 			then
 				avg=$(cat $file | grep avg | cut -d'/' -f 5)
 			fi
